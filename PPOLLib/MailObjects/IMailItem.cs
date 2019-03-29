fc726751edcd10deed8750a2082b6d33af9d883b
CXRM <brian.garstka@planplusonline.com>
2019-03-29 16:29:43 -0600

    2

---
+++ PPOLLib/MailObjects/IMailItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PPOL.MailObjects
+{
+    public interface IMailItem : IDisposable
+    {
+        string Subject { get; }
+        string Body { get; }
+        string HTMLBody { get; }
+        bool Sent { get; }
+        DateTime SentOn { get; }
+        string SenderEmailAddress { get; }
+        string SenderName { get; }
+        void AddRecipient(string emailAddress, MailRecipientKind kind);
+        IEnumerable<string> GetRecipientAddresses();
+        void Send();
+        object ComObject { get; }
+        IEnumerable<IAttachmentInfo> GetAttachments();
+        IAttachmentInfo GetSingleAttachment();
+    }
+    public interface IAttachmentInfo : IDisposable
+    {
+        string FileName { get; }
+        System.IO.Stream OpenRead();
+    }
+    
+    public enum MailRecipientKind
+    {
+        Unknown=0,
+        To=1,
+        Cc=2,
+        Bcc=3
+    }
+}
---
+++ PPOLLib/MailObjects/IOutlookApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PPOL.MailObjects
+{
+    public interface IOutlookApplication
+    {
+        IMailItem GetCurrentInspectorItem();
+        IEnumerable<IContactItem> FindContactsByEmailDisplayName(string displayName);
+        IEnumerable<string> GetPrimarySmtpAddresses();
+        //IWin32Window CurrentWindow { get; }
+    }
+
+    public interface IOutlookWindowHost
+    {
+        IWin32Window CurrentWindow { get; }
+    }
+}
